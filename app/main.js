278816f80bcfc6e879f93b1104ad2b66a7491a7d
Anandha Marthandan <Anandha Marthandan>
2016-05-24 09:59:19 -0400

    initial

---
+++ app/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//UNCOMMENT TO SEE THE ERROR
+//console.log(hz)
+console.log('welcome');
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import * as hz from '@horizon/client'
+
+//UNCOMMENT TO SEE THE ERROR
+//console.log(hz)
+
+console.log('welcome')
